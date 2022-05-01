d26a0f236b5316bcbecdc5296f20c24d2845a51a
codezart44 <100523059+codezart44@users.noreply.github.com>
2022-05-01 20:15:28 +0200

    Update Raketen.m

--- Raketen.m
+++ Raketen.m
@@ -30,212 +30,165 @@ clc
 
 
 %% b) RK4 program som löser ODE
-
+clear all
+clc
 %{
     BEGYNNELSEVÄRDEN
     t = 0
     r = H+1         phi = 0
     r' = 0          phi' = 0
 %}
-%{
-H = 6;
-h = 0.01;
-T = 5;
+
+% Test av olika starthöjder H mellan 1 och 10 (plottade)
+T = 4;
 format long;
 for i = 1:10
     H = i;
-    [status, tVecToEnd, distVecToEnd, iter] = run_simulation(H, h, T);
-    plot(tVecToEnd, distVecToEnd);
+    [tVec, rVec, ~, ~, ~] = RK4(H, T);
+    plot(tVec, rVec);
     hold on
-    disp(' ')
-    disp(status)
-    disp(' ')
-    fprintf('Distance at start: %f earth radii', H+1)
-    disp(' ')
- 
+    fprintf('Distance at start: %f earth radii \n', H+1)
 end
-%}
+yline(1)
+
 
 %% c) Beräkna tidpunkt och position för banans lägsta punkt
+clear all;
+clc;
 
 T = 5;
-H = 3.426016;
-% H = 3.426016; ger iaf minsta avstånd = 1.000000...?
+% H = 3.42600297724385;      Ger minsta höjd 1.00000...
+H = 3.426;
 
-[tMin, phiMin, rMin] = lowest_point(H, T);
-fprintf('Distance at start: %f e.r.\n', H+1)
-fprintf('Shortest distance: %f e.r.\n', rMin)
-fprintf('At angle: %f rad.\n', phiMin)
-fprintf('At time: %f h\n', tMin)
+% Skriver ut minimihöjden och plottar genererade värden mot polynomet (nästan identiska)
+[tVec, rVec, ~, ~, ~] = RK4(H, T);
+[~, LPI] = min(rVec);
+[f, t, rMin, tSpan, rSpan] = secant(LPI, tVec, rVec);
+disp('  rMin: ' + rMin)
 
-[tVec, yMtrx] = RK4(H, T);
-rData = yMtrx(:, 1);
-phiData = yMtrx(:, 2);
-
-%[coeffVec] = MKV(tVec, rData);
-c = polyfit(tVec, rData, 5)
-f = @(x) c(4)*x^0 + c(3)*x^1 + c(2)*x^2 +c(1)*x^3% + c(2)*x^4 + c(1)*x^5 + 
-for i = 1:length(tVec)
-    y(i) = f(tVec(i));
+% fitted är vektorn med höjderna (r) beräknat med f(t). 
+fitted = ones(1, length(tSpan));
+for i = 1:length(tSpan)
+    t = tSpan(i);
+    fitted(i) = f(t);
 end
-plot(tVec, y)
 
-
+% Jämförelseplot mellan rSpan och fitted
+plot(tSpan, rSpan)
+hold on
+plot(tSpan, fitted)
 
 
 %% d) Hitta gränsfall, bestämma hastighet v0 och rita bankurva.
+clear all;
+clc;
 
-% rMin = 1
-% f(H) = rMin
-% ==> f(H) - rMin = 0
-
-% Plotta HVec och rMinVec - avläsa H där rMin = 1.
-% ex. Vi vet: H=3 ger "crash" och H=4 ger "escape".
-% ==> H = 3:noggrannhet:4;
-% iterativ funktion som tar in värden på H och beräknar rMin
-
-% ngt ==> 100 datapunkter {interpolation!} ==> 10^7 datapunkter ==> mer exakt värde på H / rMin
-% ngt ==> 100 datapunkter {interpolation!} ==> koefficienter, med nogrannhet 10^-7 ==> polynom f(x) ==> sätt f(x) = 1 ==> sekantmetoden g(x) = f(x) - 1 = 0 (hitta nollställen), med nogrannhet 10^-14
-
-
+% H-värdet måste ligga mellan H3 och H4
 H3 = 3;     % definitiv krash
 H4 = 4;     % definitiv undflykt
-% Dvs H-värdet måste ligga mellan H3 och H4
+
+T = 5;
+h = 10^-2;      % Steglängd mellan H3 och H4.
 
 % Generera HVec och rMinVec
 HVec = H3:h:H4;
+rMinVec = ones(1, length(HVec));
 for i = 1:length(HVec)
     H = HVec(i);
-    [tMin, phiMin, rMin] = lowest_point(H, T);
+    [tVec, rVec, ~, ~, ~] = RK4(H, T);
+    [~, LPI] = min(rVec);                                       % Returnerar index för den lägsta punkten under flygturen.
+    [f, t, rMin, tSpan, ySpan] = secant(LPI, tVec, rVec);
     rMinVec(i) = rMin;
 end
-plot(HVec, rMinVec)
-[sdaf] = divDiff(HVec, rMinVec)
+
+% plot(HVec, rMinVec)
 
 
 
+% Ger rät linje ==> y = kx + m
+% Ser att det är då H är mellan 3.4 och 3.5
+
+% Beräkna k
+H1 = HVec(end);
+H0 = HVec(1);
+rMin1 = rMinVec(end);
+rMin0 = rMinVec(1);
+k = (rMin1 - rMin0)/(H1 - H0);
+disp("  k: " + k)
+
+% Beräkna m
+y = rMin0;
+x = H0;
+m = y - k*x;
+disp("  m: " + m)
+
+% Omskrivning av räta linjens ekvation för att räkna baklänges.
+% Vi söker H (d.v.s. x) då y (rMin) är 1.
+% y = kx+m  ==>  x = (y-m)/k
+x = @(y) (y - m)/k;
+
+H_precise = x(1);
+disp("  H*: " + H_precise)
 
 
+% Hastigheten v0 raketen sveper förbi jordytan med.
+[tVec, rVec, rPrimeVec, phiVec, phiPrimeVec] = RK4(H_precise, T);
+[~, LPI] = min(rVec);
+phiPrimeMin = phiPrimeVec(LPI);         % Gör noggrannare??
+tMin = tVec(LPI);
+rMin = rVec(LPI);
+% plot(tVec, rPrimeVec)
 
-%{
+EARTH_CIRCUMF = 4*10^4;                 % (km)
+v0 = phiPrimeMin * EARTH_CIRCUMF/(2*pi*3.6);        
 
-def divDiff(x,Y):
-    diffs = []
-    for i in range(len(Y)-1):
-        diffs.append((Y[i+1] - Y[i])/(x+1))
-    return diffs[0], diffs 
+disp("Lowest point for H* is at:")
+disp("  time t: " + tMin)
+disp("  velocity v0: " + v0)
 
-def main():
-    Y = [102, 108, 124, 156, 210, 292, 408, 564]
-    steps = 0
-    y_data = Y
-    firsts = [102]
-    for i in range(len(y_data[steps:])):
+% Ritar ut färdbanan då raketen precis sveper över trädtopparna
+% Bara fram till lägsta punkten?? Se uppgiftsbeskrivning
+plot(tVec(1:LPI), rVec(1:LPI))
+yline(1)
+% hold on
+% plot(tMin, rMin, 'rO')        % markering för lägsta punkten
 
-        if sum(y_data) == len(y_data):
-            break
+% phiPrime max 6.416286864014754 rad/h
+% LPI = 2203
+[~, phiLPI] = max(phiPrimeVec);
+disp(phiLPI)
+% Egetnligen uppnås högsta hastigheten för LPI = 2207
+disp(rPrimeVec(2203))
 
-        first, diffs = divDiff(i, y_data)
-        y_data = diffs
-        steps += 1
-        if first != 0:
-            firsts.append(first)
-    print(firsts)
-
-    x = range(len(Y))
-    y = [P(x) for x in x]
-    plot_poly(x, Y, y)
-
-main()
-
-
-%}
 
 %% Functions
 
-% Kan det vara MKV??? fortsättning följer...
-function [coeffVec] = MKV(xData, yData)
-    coeffVec = xData'\yData;
-end
-%{
-function [coeffVec] = dividedDifference(xVec, yVec)
-    maxIndex = length(xVec);
-    diffMtrx = zeros(maxIndex, maxIndex);
-    diffMtrx(:, 1) = yVec;
-    for j = 1:maxIndex
-        for i = j:maxIndex
-            dY = diffMtrx(i, j-1) - diffMtrx(i-1, j-1);
-            dX = xVec(i) - xVec(i-j+1);
-            diffMtrx(i, j) = dY/dX;
-        if i == j:
-            coeffVec(j) = diffMtrx(i, j)        % lagrar koefficienterna i en vektor
-        end
-    end
-end
-%
-function [sum] = P_(x, coeffVec, xVec)
-    sum = coeffVec(1);
-    for i = 2:length(coeffVec)
-        term = coeffVec(i);
-        for j = 1:i
-            term = term * (x - xVec(j));
-        end
-        sum = sum + term;
+
+function [f, t, y, tSpan, ySpan] = secant(LPI, tVec, yVec)
+% Använder sekantmetoden för att bestämma mer exakta minsta värden för yVec.
+    span = 10;      % interpolerar över 21 punkter närmast lägsta punkten
+    ySpan = yVec(LPI-span:LPI+span);
+    tSpan = tVec(LPI-span:LPI+span);
+    c = polyfit(tSpan, ySpan, 2);
+    f = @(t) c(1)*t^2 + c(2)*t + c(3);
+
+    n = 3;          % sekantmetoden 3 iterationer.
+    % Startgissningar
+    t0 = tVec(LPI-1);       y0 = f(t0);
+    t1 = tVec(LPI+1);       y1 = f(t1);
+    
+    for i = 1:n
+        t = t1 - (t1 - t0)*y1 / (y1 - y0);
+        y = f(t);
+
+        t0 = t1;            y0 = y1;
+        t1 = t;             y1 = y;
     end
 end
 
-% Does ones round of divdiffs
-function [divDiffVec] = divDiff(xVec, yVec)
-    for i = 1:length(xVec)-1
-        currentY = yVec(i); nextY = yVec(i+1);
-        currentX = xVec(i); nextX = xVec(i+1);
-        divDiffVec(i) = (nextY - currentY) / (nextX - currentX);
-    end
-end
 
-%}
 
-function [tMin, phiMin, rMin] = lowest_point(H, T)
-    t0 = 0;
-    h = 10^-3;
-    [tVec, yMtrx] = RK4(H, T);
-
-    % data vectors
-    rVec = yMtrx(:, 1);
-    phiVec = yMtrx(:, 3);
-    tq = t0:0.001:T;
-
-    % Splined vectors
-    rSpline = spline(tVec, rVec, tq);
-    phiSpline = spline(tVec, phiVec, tq);
-
-    % finding lowest point (t, phi, r)
-    rMin = min(rSpline);
-    i = find(rSpline==rMin);
-    tMin = tq(i);
-    phiMin = phiSpline(i);
-end
-
-function [status, tVecToEnd, distVecToEnd, iter] = run_simulation(H, T)
-    [tVec, yMtrx] = RK4(H, T);
-    status = "unknown";
-    previous_distance = inf;
-    for i = 1:length(tVec)
-        current_distance = yMtrx(i, 1);
-        tVecToEnd = tVec(1:i);
-        distVecToEnd = yMtrx(1:i, 1);
-        iter = i;
-        if current_distance <= 1
-            status = "crashed";
-            break;
-        elseif current_distance > previous_distance
-            status = "Escaped!!";
-        end
-        previous_distance = current_distance;
-    end
-end
-
-function [tVec, yMtrx] = RK4(H, T)
+function [tVec, rVec, rPrimeVec, phiVec, phiPrimVec] = RK4(H, T)
     g = 20;
     F = g/(1+H)^2;
     a = 90;
@@ -260,24 +213,19 @@ function [tVec, yMtrx] = RK4(H, T)
         v2 = yMtrx(i, 4);
 
         f1 = y_prim(u1, u2, v1, v2);
-        
-        f2 = y_prim(u1 + h/2*f1(1), u2 + h/2*f1(2), v1 + h/2*f1(3), v2 + h/2*f1(4));
-                    
+        f2 = y_prim(u1 + h/2*f1(1), u2 + h/2*f1(2), v1 + h/2*f1(3), v2 + h/2*f1(4));         
         f3 = y_prim(u1 + h/2*f2(1), u2 + h/2*f2(2), v1 + h/2*f2(3), v2 + h/2*f2(4));
-        
         f4 = y_prim(u1 + h*f3(1), u2 + h*f3(2), v1 + h*f3(3), v2 + h*f3(4));
 
         yMtrx(i+1, 1) = u1 + h/6*(f1(1) + 2*f2(1) + 2*f3(1) + f4(1));       % r(t)
         yMtrx(i+1, 2) = u2 + h/6*(f1(2) + 2*f2(2) + 2*f3(2) + f4(2));       % r'(t)
         yMtrx(i+1, 3) = v1 + h/6*(f1(3) + 2*f2(3) + 2*f3(3) + f4(3));       % phi(t)
-        yMtrx(i+1, 4) = v2 + h/6*(f1(4) + 2*f2(4) + 2*f3(4) + f4(4));       % phi'(t)
- 
-   end
+        yMtrx(i+1, 4) = v2 + h/6*(f1(4) + 2*f2(4) + 2*f3(4) + f4(4));       % phi'(t) 
+
+    end
+    rVec = yMtrx(:, 1);
+    rPrimeVec = yMtrx(:, 2);
+    phiVec = yMtrx(:, 3);
+    phiPrimVec = yMtrx(:, 4);
 end
 
-function [result] = P(x, coeffVec)
-    result = coeffVec(1)
-    for i = 2:length(coeffVec)
-        result = result + coeffVec(i) * x^(i-1)
-    end
-end
